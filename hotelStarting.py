21f7c0bfd96f5e3edc4c8886d909b525415c5db8
PL <PL@cs2k16.amcs.ws2k12.psgcs.edu>
2024-01-06 10:39:35 +0530

    printed all the neighboring distances

--- hotelStarting.py
+++ hotelStarting.py
@@ -4,11 +4,11 @@ inpf = open("C:\KLA alum data\Input data\level0.json")
  
 # returns JSON object as 
 # a dictionary
-data = json.load(inpf)
-print(type(data))
+idata = json.load(inpf)
+print(type(idata))
 # Iterating through the json
 # list
-for k,v in data.items():
+for k,v in idata.items():
     print(k,"        ",v,"\n")
  
 # Closing file
@@ -21,12 +21,54 @@ outf = open("C:\KLA alum data\Sampleoutput\level0_output.json")
  
 # returns JSON object as 
 # a dictionary
-data = json.load(outf)
-print(type(data))
+odata = json.load(outf)
+print(type(odata))
 # Iterating through the json
 # list
-for k,v in data.items():
+for k,v in odata.items():
     print(k,"        ",v,"\n")
  
 # Closing file
 outf.close()
+
+
+## starting
+
+print("idata:                         \n\n",idata)
+num_neighbours = idata["n_neighbourhoods"]
+
+for i,v in idata.items():
+    if i=="restaurants":
+        restaurant_dist = []
+        for x,z in v.items():
+            for x2,z2 in z.items():
+                if x2=="neighbourhood_distance":
+                    #restaurant_dist.append(z2)
+                    restaurant_dist = z2
+
+    if i=="neighbourhoods":
+        neighbour_dist=[]
+        
+        for i1,v2 in v.items():
+            for i3,v3 in v2.items():
+
+                if(i3=="distances"):
+                    neighbour_dist.append(v3)
+                
+#print(neighbour_dist)
+
+print("r0 -->",restaurant_dist)
+
+for i,n in enumerate(neighbour_dist):
+    print(i,"--->",n,"\n")
+
+visited=[]
+
+first_visit = restaurant_dist.index(min(restaurant_dist))
+print(first_visit)
+
+
+
+
+
+
